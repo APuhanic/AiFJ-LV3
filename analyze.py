64715bbf197a31c0f718480d3adccbae5381f1e9
APuhanic <puhanicantonio@gmail.com>
2022-12-08 11:16:00 +0100

    Popravljen ispis, dodano prepoznavanje komentara i konstanti

--- analyze.py
+++ analyze.py
@@ -10,7 +10,7 @@ from collections import Counter
 def analyze_keyword(word):
     print("('",word,"', ključna riječ)")
     #Ako je var ili fun postavlja zastavicu last_word na True da bi se sljedeca rijec mogla spremiti kao identifikator
-    if word == "var" or word == "fun":
+    if word == "var" or word == "fun" or word == "const":
         return True
     return False
 
@@ -22,7 +22,8 @@ lines=file.readlines()
 identifiers = []
 operators = []
 separators = []
-
+comments = []
+constants = []
 
 if len(lines) <= 0:
     print ("File is empty")
@@ -34,10 +35,16 @@ for line in lines:
     print("---Linija: ", line_number)
     line_number += 1
     last_word = False
+    last_word_const = False
     #Regex koristen da bi se separatori i operatori odvojili razmacima
-    words = re.sub(r"(\(|\)|;|\+|\-|\=)", r' \1 ', str(line))
+    words = re.sub(r"(\(|\)|;|\+|\-|\=|\#)", r' \1 ', str(line))
     #Regex funkcija koja razdvaja sve između razmaka i stavlja u objekt, tj. niz
     words = words.split()
+
+    if is_comment(words[0]):
+        comments.append(words[0])
+        print("('",words[0],"', komentar), (",' '.join(words[1:]),")")
+        continue
 
     #Prođi kroz svaku "rijec" i analiziaj
     for word in words:
@@ -47,15 +54,26 @@ for line in lines:
             continue #Continue nije dobro riješenje za ovo
 
         if is_operator(word):
-            separators.append(word)
+            operators.append(word)
             print("('",word,"', operator)")
             continue
 
         if is_separator(word):
-            operators.append(word)
+            separators.append(word)
             print("('",word,"', separator)")
             continue
             
+        if is_constant(word):
+            print("('",word,"', ključna riječ)")
+            last_word_const = True
+            continue
+
+        if last_word_const:
+            constants.append(word)
+            print("('",word,"', konstanta)")
+            last_word_const = False
+            continue
+
         if last_word:
             #Dodaje trenutnu rijec u listu identifikatora ako je zadnja bila var ili fun
             identifiers.append(word)
@@ -70,9 +88,12 @@ for line in lines:
         if not is_keyword(word) or not is_operator(word) or not is_separator(word):
             print("('",word,"', vrijednost)")
 
-print("Identifikatori: ", len(identifiers), identifiers)
-print(Counter(identifiers))
-print("Separatori: ", len(separators), separators)
-print(Counter(separators))
-print("Operatori: ", len(operators), operators)
-print(Counter(operators))
+print("Identifikatori: ", len(identifiers), str(Counter(identifiers))[8:-1])
+
+print("Separatori: ", len(separators), str(Counter(separators))[8:-1])
+
+print("Operatori: ", len(operators), str(Counter(operators))[8:-1])
+
+print("Komentari: ", len(comments), str(Counter(comments))[8:-1])
+
+print("Konstante: ", len(constants), str(Counter(constants))[8:-1])
